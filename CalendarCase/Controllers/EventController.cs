199633330da28958e858b7412e981aad8f605c33
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-08-01 14:10:00 +0300

    CategoryAdd Finish!

--- CalendarCase/Controllers/EventController.cs
+++ CalendarCase/Controllers/EventController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Calendar.Context;
+using CalendarCase.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CalendarCase.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult _EventMenu()
+        private readonly CalendarContext _context;
+
+        public EventController(CalendarContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEvent(string title, string color, string description = "")
+        {
+            if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(color))
+                return BadRequest("Geçersiz veri");
+
+            var newCategory = new Category
+            {
+                Name = title,
+                Color = color,
+                Description = string.IsNullOrWhiteSpace(description) ? "Otomatik oluşturuldu" : description
+            };
+
+            _context.Categories.Add(newCategory);
+            await _context.SaveChangesAsync();
+
+            return Json(new
+            {
+                success = true,
+                name = newCategory.Name,
+                color = newCategory.Color
+            });
+        }
+
     }
 }
---
+++ CalendarCase/ViewComponents/EventMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Calendar.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarCase.ViewComponents
+{
+    public class EventMenuViewComponent:ViewComponent
+    {
+        private readonly CalendarContext _context;
+
+        public EventMenuViewComponent(CalendarContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {          
+            var values = _context.Categories.ToList();
+            return View(values);
+        }
+    }
+}
